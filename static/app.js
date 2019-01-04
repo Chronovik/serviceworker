904ff85df96ec2d31c044992d282b903a4718818
Владимир Матюха <matyuha@reg.ru>
2019-01-04 19:12:17 +0300

    fix

--- static/app.js
+++ static/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function(reg) {
+    navigator.serviceWorker.register('https://chronovik.github.io/serviceworker/sw.js', { scope: 'https://chronovik.github.io/serviceworker/' }).then(function(reg) {
         // регистрация сработала
         console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch(function(error) {
